752436fdc4d8392dbf8d06f6427407556d2628d9
Lazy <2211717435@qq.com>
2023-01-26 02:30:16 +0800

    新增 退出聊天窗时还原标题

--- src/views/chat/Friend.vue
+++ src/views/chat/Friend.vue
@@ -1,7 +1,5 @@
 <template>
-    <div>
-        与 {{ $route.params.id }} {{ $route.params.name }} 聊天
-    </div>
+  <div>与 {{ $route.params.id }} {{ $route.params.name }} 聊天</div>
 </template>
 
 <script>
@@ -17,7 +15,14 @@ export default {
 
     updated() {
         this.updateWindow()
-        console.log( this.$route)
+    },
+
+    destroyed() {
+        const obj = {
+                id: null,
+                name: null
+            }
+        this.$store.commit("chat", obj);
     },
 
     methods: {
